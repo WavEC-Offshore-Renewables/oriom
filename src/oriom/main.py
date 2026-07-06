bca870b94dc9a6acead28b9c1b2e02f0d2e67de4
RiccaRock <182614401+RiccaRock@users.noreply.github.com>
2026-07-06 11:15:19 +0100

    Refactor:
    Operations builders, timeseries and statistical analysis builders

--- src/oriom/main.py
+++ src/oriom/main.py
@@ -17,12 +17,6 @@ from oriom.utils import aux_operation
 
 from oriom.domain.Inputs.Inputs import Inputs
 from oriom.domain.Metocean import Metocean
-from oriom.domain.Vessels.RovDrone import RovDrone
-from oriom.domain.Operations.InspectionSite import InspectionSite
-from oriom.domain.Operations.InspectionPort import InspectionPort
-from oriom.domain.Operations.CorrectiveMajor import CorrectiveMajor
-from oriom.domain.Operations.CorrectiveMinor import CorrectiveMinor
-from oriom.domain.Operations.OperationTow import OperationTow
 from oriom.domain.OperationsStat.CorrectiveStat import CorrectiveStat
 from oriom.domain.OperationsStat.InspectionPortStat import InspectionPortStat
 from oriom.domain.OperationsStat.InspectionSiteStat import InspectionSiteStat
@@ -33,15 +27,8 @@ from oriom.domain.Techs.Power import PVPower as PVPower
 from oriom.domain.FindElementClass import Find_Element
 from oriom.domain.Techs.Technologies import TechnologyBuilder
 from oriom.domain.Layouts.Layouts_Managers import LayoutManager
-
-from oriom.core.builders.DefineOperationTechs import Define_operation
-from oriom.core.timeseries_analysis.operation_managers.operations_inspection_site_manager import inspect_site_manager
-from oriom.core.timeseries_analysis.operation_managers.operations_tow_manager import operation_tow_manager
-from oriom.core.timeseries_analysis.operation_managers.operations_inspection_port_manager import operation_inspect_port_manager
-from oriom.core.timeseries_analysis.operation_managers.operations_major_manager import operation_major_manager
-from oriom.core.timeseries_analysis.operation_managers.operations_minor_manager import opeartion_minor_manager
 from oriom.core.timeseries_analysis.montecarlo import f_montecarlo
-
+from oriom.core.builders.operation_builder import aux_operation_builder, aux_operation_stats_builder, operation_timeseries_builder
 from oriom.core.statistical_analysis.statisticals_duration_manager import statistical_duration_manager
 from oriom.core.statistical_analysis.power_stats import average_pwind, average_pwave
 from oriom.core.statistical_analysis.final_run_statistics import return_statistics_runs
@@ -191,8 +178,15 @@ def run(config: ConfigRun | None = None):
     port = Port(
         id_= 'port_id',
         name = 'My_Port',
-        location = {'lat': inputs.tseries.site_lat["value"], 'lon': inputs.tseries.site_lon["value"]} 
-    ) """
+        location = {'lat': inputs.tseries.site_lat["value"], 'lon': inputs.tseries.site_lon["value"]}
+    )
+
+    storage = Storage(
+        id_ = 'storage_id',
+        max_space = 5
+    )
+    
+    """
 
     logging.info('--------------------\tINPUTS - METOCEAN\t--------------------')
     # Build or reuse Metocean in one call
@@ -225,15 +219,6 @@ def run(config: ConfigRun | None = None):
     metocean = Metocean.attach_power_columns(metocean = metocean, power_farm = farm_technologies.power, out_dir=dirs.run_dir)
 
 
-    logging.info('--------------------\tLAYOUT\t--------------------')
-    G_layouts = LayoutManager.build_layouts(
-        power_farm=farm_technologies.power,
-        wtg=farm_technologies.wtg,
-        wec=farm_technologies.wec,
-        pv=farm_technologies.pv,
-        graph_dir=dirs.graph_dir,
-    )
-
     logging.info('--------------------\FAILURES\t--------------------')
     # Define failure events
     failures = Failure.get_failures_from_yaml(
@@ -245,141 +230,24 @@ def run(config: ConfigRun | None = None):
             failure.fail_rate *= inputs.stats.failure_ratio_sensitivity["value"]
             logging.info(f'Failure {failure.id_} FR have been multiplied by {inputs.stats.failure_ratio_sensitivity["value"]}')
 
-    logging.info('--------------------\tINPUTS - OPERATIONS\t--------------------')
-    vessels = {}
-
-    # Define ROVs and Drones
-    rovs_drones = RovDrone.get_rovdrones_from_yaml(files.rovs_drones_file)
-
-    # Define Towing Operations
-    operations_tow = OperationTow.get_operations_from_yaml(
-        file_path = files.operations_tow_file
-    )
-
-    # Define Inspection Campaings at site
-    operations_inspect_site = InspectionSite.get_inspections_from_yaml(
-        file_path = files.operations_insp_site_file
-    )
-
-    # Define Inspection Campaings at port
-    operations_inspect_port = InspectionPort.get_inspections_from_yaml(
-        file_path = files.operations_insp_port_file,
-        towing_operations = operations_tow
-    )
-
-    # Define Major Corrective Operations
-    operations_corr_major = CorrectiveMajor.get_operations_from_yaml(
-        file_path = files.operations_corr_major_file,
-        towing_operations = operations_tow
-    )
-
-    # Define Minor Corrective Operations
-    operations_corr_minor = CorrectiveMinor.get_operations_from_yaml(
-        file_path = files.operations_corr_minor_file
-    )
-
-    total_operations = operations_tow + operations_inspect_site + operations_inspect_port + operations_corr_major + operations_corr_minor
-
-    for operation in (operations_corr_major + operations_corr_minor):
-        # Define deferred months of operation if presents
-        operation.define_months_operations()
-        # Define failure class in Operation attributes
-        aux_operation.get_failures(operation, failures)
-
-    # Create and prepare a run folder for each operation
-    for operation in total_operations:
-        aux_functions.create_run_folder_operation(
-            operation = operation,
-            operation_dir = dirs.operation_dir,
-            inputs_gen = inputs.general,
-            operation_files = Config.OPERATION_FILES
-        )
-
-        # Populate Operations with vessels
-        if getattr(operation, 'vessel1_id', None):
-            Define_operation.define_vessels(
-                operation = operation,
-                file_vessels = files.vessels_file,
-                file_fuel_cons = files.vessels_fuel_cons_file,
-                file_load_factor = files.vessels_load_factor_file,
-                file_fuel_density = files.vessels_fuel_density_file,
-                vessels = vessels
-            )
-
-        # Populate Operations with ROV
-        if getattr(operation, 'rov_drone', None):
-            Define_operation.define_rovs(
-                operation=operation,
-                rovs_drones=rovs_drones,
-            )
-
-    # Populate Operations with device at port storage and simultaneus operations
-    for operations_list, inspection in zip(
-        (operations_inspect_port, operations_corr_major),
-        (True, False)
-    ):
-        for operation in operations_list:
-            aux_operation.define_device_at_port(
-                oper=operation,
-                wtg=farm_technologies.wtg,
-                wec=farm_technologies.wec,
-                pv=farm_technologies.pv,
-                inspection=inspection
-            )
-
-    # Populate the inspection level if not defined previously
-    for operation in (operations_inspect_port + operations_inspect_site):
-        operation.define_level(G_layouts = G_layouts)
-
-    # Check minor operation durations
-    for operation in operations_corr_minor:
-        if operation.duration_net >= inputs.tseries.shift_duration["value"]:
-            raise ValueError("OperMinor: Duration too long, define the operation as a major", operation.id)
-
-    for operation in operations_tow:
-        if getattr(operation, "addition_op_tow", None):
-            operation.define_previous_op_tow(operations_corr_major)
-
-    # Populate Major Corrective ant Tow Operations Operations with activities
-    for operation in (operations_corr_major + operations_tow):
-        op_dir = os.path.join(dirs.operation_dir, operation.id)
-        # Check if there is already an activities file
-        if check_files and check_files.check_file_exists(path=op_dir, file_name='activities.csv'):
-            aux_operation.recycle_activities(
-                operation = operation,
-                dir=op_dir,
-                file_name='activities',
-                tow_op = operation in operations_tow
-            )
-        else:
-            time_between_devices = inputs.tseries.find_time_between_devices(operation_obj_id = operation.id)
-
-            aux_operation.define_activities(
-                operation = operation,
-                file_activities = files.operations_activities_file,
-                distance_to_site = inputs.tseries.distance["value"],
-                transit_between_devices = time_between_devices,
-                tow_op = operation in operations_tow
-            )
-            operation.activities[0].save_activities_as_csv(operation, os.path.join(op_dir, 'activities.csv'))
-
     # Check if all the levels defined are associated to a component in the graph
-    for operation_type in [operations_inspect_port, operations_inspect_site]:
-        aux_operation.level_component_check(Gs = G_layouts, operations = operation_type)
+    aux_operation.level_component_check(Gs = G_layouts, operations = failures, failure = True)
+
+    logging.info('--------------------\tINPUTS - OPERATIONS\t--------------------')
+    operations = aux_operation_builder(
+        Config = Config,
+        inputs = inputs,
+        files = files,
+        dirs = dirs,
+        failures = failures,
+        farm_technologies = farm_technologies,
+        G_layouts = G_layouts
+    )
 
     # Create list of vessels and mother_vessels
-    vessels = list(vessels.values())
+    vessels = list(operations['vessels'].values())
     mother_vessels = [v for v in vessels if v.mother_vessel]
 
-    # Save operation attributes as YAML files
-    for operation in total_operations:
-        op_dir = os.path.join(dirs.operation_dir, operation.id)
-        # Check if there is already an attributes file
-        if check_files and check_files.check_file_exists(path=op_dir, file_name='attributes.yaml'):
-            continue
-        operation.to_yaml(op_dir)
-
-    aux_operation.operation_check_identities(total_operations)
 
     logging.info('--------------------\tDERIVED INPUTS\t--------------------')
     # Statistical analysis of the power fro wtg and wec
@@ -393,8 +261,7 @@ def run(config: ConfigRun | None = None):
             out_dir=os.path.join(os.getcwd(), dirs.run_dir)
     )
 
-    # Check if all the levels defined are associated to a component in the graph
-    aux_operation.level_component_check(Gs = G_layouts, operations = failures, failure = True)
+
 
     # Generate random timesteps to be analysed
     timesteps, _ = f_montecarlo(
@@ -408,49 +275,16 @@ def run(config: ConfigRun | None = None):
     logging.info('--------------------\tTIMESERIES ANALYSIS\t--------------------')
     logging.info('---------------------------------------------------------------')
 
-    operation_tow_manager(
-        operation_dir = dirs.operation_dir,
-        df_metocean = metocean.df_timeseries,
-        max_wait = inputs.tseries.max_wait["value"],
-        operations_tow = operations_tow,
+    operation_timeseries_builder(
+        inputs = inputs,
+        dirs = dirs,
+        operations = operations,
+        metocean = metocean,
+        metocean_port = metocean_port,
+        metocean_tow = metocean_tow,
+        metocean_tow_distance = metocean_tow_distance,
         timesteps = timesteps,
         Config = Config,
-        inputs_tseries = inputs.tseries,
-        metocean_tow = metocean_tow,
-        metocean_tow_distance = metocean_tow_distance
-    )
-
-    inspect_site_manager(
-        operation_dir = dirs.operation_dir,
-        df_metocean = metocean.df_timeseries,
-        operations_inspect_site = operations_inspect_site,
-        inputs_tseries = inputs.tseries,
-        Config = Config
-    )
-
-    operation_inspect_port_manager(
-        operation_dir = dirs.operation_dir,
-        df_metocean = metocean_port.df_timeseries,
-        duration_shift = inputs.tseries.shift_duration["value"],
-        operations_inspect_port = operations_inspect_port
-    )
-
-    operation_major_manager(
-        operation_dir = dirs.operation_dir,
-        df_metocean = metocean.df_timeseries,
-        df_metocean_port = metocean_port.df_timeseries,
-        operations_corr_major = operations_corr_major,
-        inputs_tseries = inputs.tseries,
-        Config = Config,
-        timesteps = timesteps
-    )
-
-    opeartion_minor_manager(
-        operation_dir = dirs.operation_dir,
-        df_metocean = metocean.df_timeseries,
-        operations_corr_minor = operations_corr_minor,
-        inputs_tseries = inputs.tseries,
-        Config = Config
     )
 
 
@@ -458,59 +292,11 @@ def run(config: ConfigRun | None = None):
     logging.info('---------------------------------------------------------------')
     logging.info('--------------------\tSTATISTICAL ANALYSIS\t----------------')
     logging.info('---------------------------------------------------------------')
-
-    statistical_duration_manager(
-        operation_dir = dirs.operation_dir,
-        total_operations = total_operations,
-        inputs_stats = inputs.stats
-    )
-
-    # Fill the operations_stats with the maximum value of percentile
-    logging.info('--------------------\tOperations statistics\t----------------')
-    percentiles = {"pmax": inputs.stats.percentile_max["value"], "pmain": inputs.stats.percentile_main["value"]}
-
-    operations_tow_stats = {}
-    for key, perc in percentiles.items():
-        operations_tow_stats[key] = OperationsTowStat.get_towing_statistics(
-            operations=operations_tow,
-            PERCENTILE=perc,
-            run_dir=dirs.operation_dir,
-        )
-
-    operations_corrective_stats = {}
-    for key, perc in percentiles.items():
-        operations_corrective_stats[key] = CorrectiveStat.get_corrective_statistics(
-            operations = operations_corr_major+operations_corr_minor,
-            operations_tow_stat = operations_tow_stats[key],
-            PERCENTILE = perc,
-            run_dir = dirs.operation_dir,
-        )
-
-    inspections_site_stats = {}
-    for key, perc in percentiles.items():
-        inspections_site_stats[key] = InspectionSiteStat.get_inspection_statistics(
-            insepctions_site=operations_inspect_site,
-            PERCENTILE=perc,
-            run_dir=dirs.operation_dir,
-        )
-
-    inspections_port_stats = {}
-    for key, perc in percentiles.items():
-        inspections_port_stats[key] = InspectionPortStat.get_inspection_statistics(
-            insepctions_port = operations_inspect_port,
-            PERCENTILE = perc,
-            run_dir = dirs.operation_dir,
-            n_port_inspections = {
-                prefix: getattr(tech, 'n_device_at_port', 0) for prefix, tech in zip(["ofw", "owc", "opv"], [farm_technologies.wtg, farm_technologies.wec, farm_technologies.pv])
-            },
-            operations_tow_stat = operations_tow_stats[key],
-            shift = inputs.tseries.shift_duration["value"]
-        )
-
-    find_element = Find_Element.create(
-        operations = operations_tow + operations_inspect_site + operations_inspect_port + operations_corr_major + operations_corr_minor,
-        operations_stats = operations_tow_stats['pmain'] + inspections_site_stats['pmain'] + inspections_port_stats['pmain'] + operations_corrective_stats['pmain'],
-        operations_stats_pmax = operations_tow_stats['pmax'] + inspections_site_stats['pmax'] + inspections_port_stats['pmax'] + operations_corrective_stats['pmax'],
+    operations_stats, find_element = aux_operation_stats_builder(
+        inputs = inputs,
+        dirs = dirs,
+        farm_technologies = farm_technologies,
+        operations = operations,
         vessels = vessels,
         failures = failures
     )
@@ -538,10 +324,10 @@ def run(config: ConfigRun | None = None):
             farm_technologies = farm_technologies,
             results_dict = results_container,
             failures = failures,
-            operations_tow_stats = operations_tow_stats,
-            inspections_port_stats = inspections_port_stats,
-            inspections_site_stats = inspections_site_stats,
-            operations_corrective_stats = operations_corrective_stats,
+            operations_tow_stats = operations_stats['operations_tow_stats'],
+            inspections_port_stats = operations_stats['inspections_port_stats'],
+            inspections_site_stats = operations_stats['inspections_site_stats'],
+            operations_corrective_stats = operations_stats['operations_corrective_stats'],
             vessels = vessels,
             mother_vessels = mother_vessels,
             G_layouts = G_layouts,
@@ -570,7 +356,7 @@ def run(config: ConfigRun | None = None):
         electricity_cost_dict = inputs.cost.electricity_price_dict,
         n_runs = inputs.general.number_runs["value"],
         vessels = vessels,
-        operations_total = total_operations,
+        operations_total = operations['total_operations'],
         save_dir = dirs.result_dir_avg
     )
 
@@ -581,12 +367,7 @@ def run(config: ConfigRun | None = None):
         export_st_package(
             package_dir=r"C:\Users\RiccardoMeda\temp",
             Config=Config,
-            operations_stats = {
-                'operations_tow_stats': operations_tow_stats,
-                'operations_corrective_stats': operations_corrective_stats,
-                'inspections_port_stats': inspections_port_stats,
-                'inspections_site_stats': inspections_site_stats,
-            },
+            operations_stats = operations_stats,
             overwrite = True
         )
 
